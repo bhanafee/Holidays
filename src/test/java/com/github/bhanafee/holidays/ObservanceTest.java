6de7eb37bf0f471bd6f153d1fa66e864b20d17ec
Brian Hanafee <bhanafee@gmail.com>
2021-10-16 10:07:47 -0700

    Add TestNG initial tests

--- src/test/java/com/github/bhanafee/holidays/ObservanceTest.java
+++ src/test/java/com/github/bhanafee/holidays/ObservanceTest.java
@@ -1,7 +1,47 @@
 package com.github.bhanafee.holidays;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
-class ObservanceTest {
+import java.time.LocalDate;
+import java.util.Optional;
+import java.util.function.Function;
+
+import static org.testng.Assert.*;
+
+@Test
+public class ObservanceTest {
+    final String NAME = "name";
+    final LocalDate DATE = LocalDate.now();
+    Function<Optional<LocalDate>, Optional<Observance>> named;
+
+    @BeforeClass
+    public void setup() {
+        named = Observance.named(NAME);
+    }
+
+    public void testNamedEmpty() {
+        assertEquals(named.apply(Optional.empty()), Optional.empty(), "Empty input should produce empty output");
+    }
+
+    public void testNamed() {
+        final Optional<Observance> result = named.apply(Optional.of(DATE));
+        assertTrue(result.isPresent(), "Non-empty input should produce non-empty result");
+        assertEquals(result.get().getName(), NAME, "Name should pass through");
+        assertEquals(result.get().getDate(), DATE, "Date should pass through");
+    }
+
+    public void testNamedNullInput() {
+        assertEquals(named.apply(null), Optional.empty(), "Null input should produce empty output");
+    }
+
+    public void testNamedNullName() {
+        Function<Optional<LocalDate>, Optional<Observance>> test = Observance.named(null);
+        final Optional<Observance> result = test.apply(Optional.of(DATE));
+
+        assertTrue(result.isPresent(),"Null name should produce results");
+        assertTrue(result.get().getName().isEmpty(),
+                "Null name should produce empty string in output");
+    }
 
 }
